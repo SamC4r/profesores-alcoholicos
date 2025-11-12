124ff9258e755048e419ffd92b542ed4fbde2649
SamC4r <locoporlosrubiks@gmail.com>
2025-11-12 18:30:06 +0100

    jiji

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { birthdayClientPlugin } from "@/db/birthday/client"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL!,
     plugins: [ 
         usernameClient(),
         birthdayClientPlugin()
